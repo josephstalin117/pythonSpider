5ce3418846cb1a426fe9717053489d09fada30c9
josephstalin <lyz2356002@gmail.com>
2015-01-11 15:53:47 +0800

    add compare_news and insert_news module

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import ssmysql
 import spider
+
+# @todo need deploy the ssmysql and spider
+news = spider.get_news(
+    'http://jwc.sdibt.edu.cn/SmallClass.asp?BigClassName=%D0%C5%CF%A2%B7%A2%B2%BC&SmallClassName=%D0%C2%CE%C5%B6%AF%CC%AC')
+
+news = ssmysql.compare_news(news)
+ssmysql.insert_news(news)
--- ssmysql.py
+++ ssmysql.py
@@ -5,54 +5,52 @@ import time
 
 
 def compare_news(news):
-    # @todo to compare the date of the news
     db = Db()
     # the index list of expired news
     expired = 0
     for i in range(len(news)):
-        if time.mktime(time.strptime(str(news[i][4]), '%Y-%m-%d')) < db.lastTime:
+        if time.mktime(time.strptime(str(news[i][4]), '%Y-%m-%d')) <= db.lastTime:
             expired = i
             break
-    print expired
     del news[expired:]
     return news
+
+
+def insert_news(news):
+    db = Db()
+    return db.insert_news(news)
 
 
 class Db:
     def __init__(self):
         self.title = []
-        self.times = []
         self.lastTime = ''
         self.lastTimeIndex = ''
+        self.lastTitle = []
         self.result = []
         try:
-            self.conn = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='ssnews', port=3306)
+            self.conn = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='ssnews', port=3306,
+                                        charset="utf8")
             self.cur = self.conn.cursor()
             self.search_latest_time()
         except MySQLdb.Error, e:
             print "Mysql Error %d: %s" % (e.args[0], e.args[1])
 
     def search_latest_time(self):
-        self.cur.execute("select title,time from news")
+        self.cur.execute("SELECT time FROM news WHERE time IN (SELECT MAX(time) FROM news)")
         self.result = self.cur.fetchall()
         for row in self.result:
-            self.title.append(row[0])
-            self.times.append(time.mktime(time.strptime(str(row[1]), '%Y-%m-%d %H:%M:%S')))
-        self.lastTime = max(self.times)
-        self.lastTimeIndex = self.times.index(max(self.times))
-
-    # @todo compare the title of same date to fliter the news
-    def compare_title(self):
-        return
+            print row[0]
+            self.lastTime = time.mktime(time.strptime(str(row[0]), '%Y-%m-%d %H:%M:%S'))
+        print self.lastTime
 
     # @todo insert the news list
-    def insert_news(self, title, content, tag, url, time):
+    def insert_news(self, news):
         sql = "INSERT INTO news(title,content,tag,url,time) values(%s,%s,%s,%s,%s)"
-        n = self.cur.execute(sql, (title, content, tag, url, time))
-        print n
-        id = self.cur.lastrowid
+        for i in news:
+            self.cur.execute(sql, (i[0], i[1], i[2], i[3], i[4]))
         self.conn.commit()
-        return id
+        return self.cur.lastrowid
 
     def __del__(self):
         self.cur.close()
